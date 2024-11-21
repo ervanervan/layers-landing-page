2b41d5b3b5c5a680bbf710d1bc7f727d148f3ca2
ervanervan <ervanervan040@gmail.com>
2024-11-21 18:50:17 +0700

    update: Introduction section

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -4,12 +4,12 @@ const text = `You're racing to create exceptional work, but traditional design t
 
 export default function Introduction() {
   return (
-    <section className="py-24">
+    <section className="py-28 lg:py-40">
       <div className="container">
         <div className="flex justify-center">
           <Tag>Introducing Layers</Tag>
         </div>
-        <div className="text-4xl text-center font-medium mt-10">
+        <div className="text-4xl md:text-6xl lg:text-7xl text-center font-medium mt-10">
           <span>Your creative process deserves better.</span>{" "}
           <span className="text-white/15">{text}</span>
           <span className="text-lime-400 block">
