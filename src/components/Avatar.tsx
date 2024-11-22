f666d16b72a2f3454e6193330d103ba539ebf61e
ervanervan <ervanervan040@gmail.com>
2024-11-22 14:18:45 +0700

    update: Avatar component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,5 +3,13 @@ import { twMerge } from "tailwind-merge";
 
 export default function Avatar(props: HTMLAttributes<HTMLDivElement>) {
   const { className, children, ...otherProps } = props;
-  return <div className={twMerge("size-20", className)} {...otherProps}></div>;
+  return (
+    <div
+      className={twMerge(
+        "p-1 bg-neutral-900 size-20 rounded-full overflow-hidden border-4 border-blue-500",
+        className
+      )}
+      {...otherProps}
+    ></div>
+  );
 }
