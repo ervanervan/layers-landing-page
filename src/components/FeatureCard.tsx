c98df0c456ea5a582c51712fb929c94a6caf1d46
ervanervan <ervanervan040@gmail.com>
2024-11-22 14:05:53 +0700

    update: FeatureCard component

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -5,11 +5,11 @@ export default function FeatureCard(props: {
 }) {
   const { title, description, children } = props;
   return (
-    <div>
-      <div>{children}</div>
+    <div className="bg-neutral-900 border border-white/10 p-6 rounded-3xl">
+      <div className="aspect-video">{children}</div>
       <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
+        <h3 className="text-3xl font-medium mt-6">{title}</h3>
+        <p className="text-white-50 mt-2">{description}</p>
       </div>
     </div>
   );
