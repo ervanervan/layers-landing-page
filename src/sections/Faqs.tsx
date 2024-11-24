15533511947ebcbe2e7acadf221e98d97e7fd7ac
ervanervan <ervanervan040@gmail.com>
2024-11-24 10:54:44 +0700

    update: navigation Faqs section

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -35,7 +35,7 @@ const faqs = [
 export default function Faqs() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   return (
-    <section className="py-24">
+    <section id="faqs" className="py-24">
       <div className="container">
         <div className="flex justify-center">
           <Tag>Faqs</Tag>
